ca5046475efac0f3053b4ca40d9a415c55cd87e9
Richard Schloss <richard.schloss@blueinnovationsgroup.com>
2023-07-08 17:07:34 -0400

    Added ubuntugis apt repo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+add-apt-repository ppa:ubuntugis/ppa
 apt update
 apt install -y python3 python3-pip python3-venv build-essential wget \
-  imagemagick xmlstarlet gdal-bin python3-gdal libgdal-dev
+  imagemagick xmlstarlet gdal-bin python3-gdal libgdal-dev python-software-properties
 pip install Pipfile
 pip install wand

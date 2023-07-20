6707c73922955699f6285cb0e8e92d38934a8592
Richard Schloss <richard.schloss@blueinnovationsgroup.com>
2023-07-20 12:50:33 -0400

    Moved out the setup logic to the parent setup script

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt update
-apt install -y python3 python3-pip build-essential wget \
-  imagemagick xmlstarlet gdal-bin python3-gdal libgdal-dev # python-software-properties
-pip install Pipfile
-pip install wand
